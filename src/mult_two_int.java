d462ef1befa53e51ee598610e2b3c121f24427e8
jojopak <pak.j.jedi@gmail.com>
2018-10-05 11:06:19 -0700

    adding mult_two_int files

---
+++ src/mult_two_int.java
@@ -0,0 +1,13 @@
+
+public class mult_two_int {
+
+	public static void main(String[] args) {
+		int result = mult_two_int(1, 100);
+		System.out.println("result: " + result);
+
+	}
+	public static int mult_two_int(int num1, int num2) {
+		return num1* num2;
+	}
+
+}
